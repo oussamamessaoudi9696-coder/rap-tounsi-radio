42874d75afd9dd26d49dbc66befa896c1b1903b4
oussamamessaoudi9696-coder <oussamamessaoudi9696@gmail.com>
2026-02-22 00:42:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
 });
 
 client.once('ready', () => {
   console.log('Rap Tounsi Radio Bot is Online!');
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.content === '+join') {
+    if (message.member.voice.channel) {
+
+      joinVoiceChannel({
+        channelId: message.member.voice.channel.id,
+        guildId: message.guild.id,
+        adapterCreator: message.guild.voiceAdapterCreator,
+      });
+
+      message.reply('دخلت للـ Voice ✅');
+
+    } else {
+      message.reply('ادخل انت للـ Voice الأول ❌');
+    }
+  }
 });
 
 client.login(process.env.TOKEN);
